51705897c782939e0c7100fae1268eebed70dd5d
venustuspl <venustus.pl@gmail.com>
2020-04-11 17:20:15 +0200

    0.1

---
+++ src/main/java/pl/venustus/MoneyChanger/MoneyChangerApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.MoneyChanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyChangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/MoneyChanger/MoneyChangerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.MoneyChanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyChangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
