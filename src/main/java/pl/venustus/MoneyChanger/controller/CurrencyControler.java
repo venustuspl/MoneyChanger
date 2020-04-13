46f43ebda405087ccb502bc8e5267985973d2e1c
venustuspl <venustus.pl@gmail.com>
2020-04-13 12:53:15 +0200

    0.5

--- src/main/java/pl/venustus/MoneyChanger/controller/CurrencyControler.java
+++ src/main/java/pl/venustus/MoneyChanger/controller/CurrencyControler.java
@@ -1,26 +1,26 @@
 package pl.venustus.MoneyChanger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import pl.venustus.MoneyChanger.domain.Currency;
+
+import java.util.Arrays;
 
 @Controller
 public class CurrencyControler {
 
-    @Autowired
-    Currency c;
+    public static final String uri = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
 
-    private String url = "http://api.nbp.pl/api/exchangerates/tables/a";
-
-    @GetMapping("/")
+    @GetMapping("/all")
     public String hello(){
 
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(url, String.class);
+        String result = restTemplate.getForObject(uri, String.class);
         return result;
+       // System.out.println(result);
     }
-
 
 }
