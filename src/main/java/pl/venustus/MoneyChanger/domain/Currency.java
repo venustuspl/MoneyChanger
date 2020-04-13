bc1ae3252bdc7ccb7a5d51d9d996e378740a5291
venustuspl <venustus.pl@gmail.com>
2020-04-13 10:46:45 +0200

    0.2

---
+++ src/main/java/pl/venustus/MoneyChanger/domain/Currency.java
@@ -0,0 +1,27 @@
+package pl.venustus.MoneyChanger.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String code;
+    private BigDecimal mid;
+
+
+}
