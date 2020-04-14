de096d2d6e532fb6bf9d7c23d32fb52f7d83cc34
venustuspl <venustus.pl@gmail.com>
2020-04-14 22:01:18 +0200

    1.4

--- src/main/java/pl/venustus/MoneyChanger/Service/Data.java
+++ src/main/java/pl/venustus/MoneyChanger/Service/Data.java
@@ -5,11 +5,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.venustus.MoneyChanger.domain.DayTableDto;
 
 import java.util.Arrays;
 
+@Service
 public class Data {
 
     private static final String uri = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
--- src/main/java/pl/venustus/MoneyChanger/controller/CurrencyControler.java
+++ src/main/java/pl/venustus/MoneyChanger/controller/CurrencyControler.java
@@ -1,33 +1,31 @@
 package pl.venustus.MoneyChanger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
-import pl.venustus.MoneyChanger.domain.Currency;
+import pl.venustus.MoneyChanger.Service.Data;
+import pl.venustus.MoneyChanger.domain.DayTableDto;
 
-import java.util.Arrays;
+import java.io.IOException;
 
 
 @Controller
 public class CurrencyControler {
 
-    public static final String uri = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+    @Autowired
+    Data data;
 
-    @GetMapping("/all")
-    public void hello(){
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+    @GetMapping("/lastdaytable")
+    public DayTableDto hello() {
+        return Data.getLastDayTableDto();
+    }
 
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<String>("currency", headers);
+    @GetMapping
+    public String getMap(Model model) throws IOException {
+        model.addAttribute("lastdaytable", Data.getLastDayTableDto().getNo());
 
-        ResponseEntity<Currency[]> result = restTemplate.exchange(uri, HttpMethod.GET, entity, Currency[].class);
-
-        System.out.println(result);
+        return "index";
 
     }
 
